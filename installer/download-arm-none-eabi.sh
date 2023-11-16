7830102ccc4649b699566d879b1e05c43fae3e7a
zhitm <zhtinuhina.maria@gmail.com>
2023-11-16 18:57:31 +0300

    fixed downloading arm-none-eabi script

--- installer/download-arm-none-eabi.sh
+++ installer/download-arm-none-eabi.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/bash -x
-sudo apt install wget
 help()
 {
     echo "Usage: download-arm-none-eabi.sh [ -p | --path ] /path/to/dir
                [ -h | --help  ]"
     exit 0
 }
-PATH="/home/$USER"
+
+PATH_TO_INSTALL="/home/$USER"
 
 
 install_arm()
 {
-wget https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
+# wget https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-
+# toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
 
-tar -xf arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz -C $PATH
-echo "arm-none-eabi installed at $PATH"
+wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
+
+tar -xf arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz -C $PATH_TO_INSTALL
+echo "arm-none-eabi installed at $PATH_TO_INSTALL"
 }
 
 
@@ -22,7 +25,7 @@ echo "arm-none-eabi installed at $PATH"
 
 if [ $# -eq 0 ]
 then
-    read -r -p "Do you want to install arm-none-eabi at $PATH? [y/N] " response
+    read -r -p "Do you want to install arm-none-eabi at $PATH_TO_INSTALL? [y/N] " response
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
     then
        install_arm
@@ -36,10 +39,10 @@ fi
 
 case "$1" in
 -p | --path )
-    PATH=$2
-    if [[ ! -d "$PATH" ]]
+    PATH_TO_INSTALL=$2
+    if [[ ! -d "$PATH_TO_INSTALL" ]]
     then
-        echo "$PATH doesn't exists on your filesystem."
+        echo "$PATH_TO_INSTALL doesn't exists on your filesystem."
     else
         install_arm
     fi
